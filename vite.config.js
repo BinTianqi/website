46a4b15579e543206d751b61140118a1f78a7ff9
BinTianqi <bintianqi512@gmail.com>
2025-03-04 21:14:57 +0800

    Optimize layout

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ export default {
     build: {
         rollupOptions: {
             input: {
+                "Home": "index.html",
                 "OwnDroid": "apps/OwnDroid/index.html"
             }
         },
